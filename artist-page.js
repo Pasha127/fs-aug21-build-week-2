d8be6da05ae5ab67d20946d551fb8d9c8f7f6912
Pasha127 <pasha.account@gmail.com>
2022-08-31 12:18:06 +0200

    started artist page

---
+++ artist-page.js
@@ -0,0 +1,50 @@
+const urlParams = new URLSearchParams(window.location.search);
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a5e347aa3emsh83ddf0a8da0766ep13759cjsn26ffa1895f30',
+        'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+    }
+};
+
+const getAlbum = () => {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${urlParams.get('artist-id')}`, options)
+    .then(response => response.json())
+    .then(album => {
+        let albumCover = document.getElementById("album-cover");
+        let artistInfo = document.getElementById("artist")
+        let trackslist = document.getElementById("track-list")
+      
+            console.log(album)
+            albumCover.innerHTML = `<img height="150vh" width="100%" src=${album.cover_medium}
+            alt="this is photo">`
+            
+            artistInfo.innerHTML =`   <div>
+            <h6 class="text-white">album</h6>
+            <h2 class="text-white">${album.title}</h2>
+            <div class="d-flex">
+            <img class="small-img" src=${album.artist.picture}
+             alt="photo">
+             <h6 class="text-white"><strong>${album.artist.name}</strong><strong>.</strong><strong>.</strong>${album.duration} min </h6>
+            </div>`
+         for (let i = 0; i < album.tracks.data.length; i++) {
+            const track = album.tracks.data[i];
+            console.log(track)
+            trackslist.innerHTML +=`<div class="row justify-content-between" >
+            <div class="d-flex">
+            <div class="mt-2 mr-5">${i+1}</div>
+            <div>
+            <div>${track.title}</div>
+            <div>${track.artist.name}</div>
+            </div>
+            </div>
+            <div class="mr-4">${track.duration} min</div>        
+            </div>`
+         }
+        
+    })
+    .catch(err => console.log(err))}
+    window.onload = ()=>{
+        getAlbum();
+    }
+
