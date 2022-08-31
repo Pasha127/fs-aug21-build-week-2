1b29265672e30d20f1b607c387a70520af629fc5
Hatem Mahmoud <hatemhindawi1999@gmail.com>
2022-08-31 09:05:15 +0200

    album-js

--- album-page.js
+++ album-page.js
@@ -12,16 +12,33 @@ const getAlbum = () => {
     .then(album => {
         let albumCover = document.getElementById("album-cover");
         let artistInfo = document.getElementById("artist")
+        let trackslist = document.getElementById("track-list")
       
             console.log(album)
-            albumCover.innerHTML = `<img height="150vh" src=${album.cover_medium}
+            albumCover.innerHTML = `<img height="150vh" width="100%" src=${album.cover_medium}
             alt="this is photo">`
-            artistInfo.innerHTML = `<h6 class="text-white">album</h6>
+            artistInfo.innerHTML =`   <div>
+            <h6 class="text-white">album</h6>
             <h2 class="text-white">${album.title}</h2>
             <div class="d-flex">
-            <img class="small-img" src=${album.artist.picture} 
+            <img class="small-img" src=${album.artist.picture}
              alt="photo">
-             <h6 class="text-white"><strong>${album.artist.name}</strong>${album.duration} min</h6>`
+             <h6 class="text-white"><strong>${album.artist.name}</strong><strong>.</strong><strong>.</strong>${album.duration} min </h6>
+            </div>`
+         for (let i = 0; i < album.tracks.data.length; i++) {
+            const track = album.tracks.data[i];
+            console.log(track)
+            trackslist.innerHTML +=`<div class="row" >
+            <div class="mt-2 mr-5">${i+1}</div>
+            <div>
+            <div>${track.title}</div>
+            <div>${track.artist.name}</div>
+            </div>
+        </div>
+        <div class="">${track.duration}</div>
+             </div>
+             </div>`
+         }
         
       
     
